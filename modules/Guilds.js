6c39a779e7c5cda484c6a210bbaa7fda79115e54
LockBlock-dev <LockBlock-dev@protonmail.com>
2021-07-05 10:20:13 +0200

    choice to add invites or not

--- modules/Guilds.js
+++ modules/Guilds.js
@@ -15,6 +15,8 @@ const setMax = (list) => {
 }
 
 module.exports = async (token) => {
+    const addInvites = require('readline-sync').keyInYNStrict("Getting invites is a very long process. Do you want to try to get an invite for each guild ?")
+
     var list = await request(token, "/users/@me/guilds", "GET")
 
     if (debug) {
@@ -30,45 +32,53 @@ module.exports = async (token) => {
         id = list[e].id
         found = false
 
-        channels = await request(token, `/guilds/${id}/channels`, "GET")
+        if (addInvites) {
+            channels = await request(token, `/guilds/${id}/channels`, "GET")
 
-        if (debug) {
-            channels.success ? console.log(`Channels: ${channels.success}`) : console.log(`Channels: ${channels.success} => ${JSON.stringify(channels.data)}`)
-        }
-
-        channels = channels.data
-
-        for (var c in channels) {
-            if (!found) {
-                var body = {
-                    "validate": null,
-                    "max_age": 0,
-                    "max_uses": 0,
-                    "target_type": null,
-                    "temporary": false
-                }
-                invite = await request(token, `/channels/${channels[c].id}/invites`, "POST", body)
-
-                if (debug) {
-                    invite.success ? console.log(`Invite: ${invite.success}`) : console.log(`Invite: ${invite.success} => ${JSON.stringify(invite.data)}`)
-                }
-
-                invite = invite.data
-
-                invite.code ? found = true : found = false
-
-                await require('../utils/sleep')(3000)
+            if (debug) {
+                channels.success ? console.log(`Channels: ${channels.success}`) : console.log(`Channels: ${channels.success} => ${JSON.stringify(channels.data)}`)
             }
+
+            channels = channels.data
+
+            for (var c in channels) {
+                if (!found) {
+                    var body = {
+                        "validate": null,
+                        "max_age": 0,
+                        "max_uses": 0,
+                        "target_type": null,
+                        "temporary": false
+                    }
+                    invite = await request(token, `/channels/${channels[c].id}/invites`, "POST", body)
+
+                    if (debug) {
+                        invite.success ? console.log(`Invite: ${invite.success}`) : console.log(`Invite: ${invite.success} => ${JSON.stringify(invite.data)}`)
+                    }
+
+                    invite = invite.data
+
+                    invite.code ? found = true : found = false
+
+                    await require('../utils/sleep')(3000)
+                }
+            }
+
+            invite.code ? invite = `discord.gg/${invite.code}` : invite = "NO_INVITE"
+
+            guilds += `${name}${" ".repeat((max - name.length) + 1)}: ${id} : ${invite}\n`
+            json.push({
+                name: name,
+                id: id,
+                invite: invite
+            })
+        } else {
+            guilds += `${name}${" ".repeat((max - name.length) + 1)}: ${id}\n`
+            json.push({
+                name: name,
+                id: id
+            })
         }
-
-        invite.code ? invite = `discord.gg/${invite.code}` : invite = "NO_INVITE"
-
-        guilds += `${name}${" ".repeat((max - name.length) + 1)}: ${id} : ${invite}\n`
-        json.push({
-            name: name,
-            id: id,
-            invite: invite
-        })
     }
 
     fs.writeFileSync("./results/guilds.txt", guilds)
