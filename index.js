75ae61c48958e6ba33e85e7845087b23b840a37c
LockBlock-dev <LockBlock-dev@protonmail.com>
2021-07-08 09:40:09 +0200

    regex check for tokens normal/MFA

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-const modules = require('./utils/loader')()
+const token = require('./config.json').token
 
-console.log("\nDiscord Exporters - To begin, select a module")
+const tokenRe  = /[A-Za-z\d]{24}\.[\w-]{6}\.[\w-]{27}/
+const MFAtokenRe = /mfa\.[A-Za-z\d-_]{84}/
 
-const i = require('readline-sync').keyInSelect(modules, "Available modules :")
+if (!tokenRe.test(token) && !MFAtokenRe.test(token)) {
+    console.log("\nDiscord Exporters - INVALID TOKEN PROVIDED")
+} else {
+    const modules = require('./utils/loader')()
 
-i > -1 ? require(`./modules/${modules[i]}`)(require('./config.json').token) : null
+    console.log("\nDiscord Exporters - To begin, select a module")
+
+    const i = require('readline-sync').keyInSelect(modules, "Available modules :")
+
+    i > -1 ? require(`./modules/${modules[i]}`)(token) : null
+}
